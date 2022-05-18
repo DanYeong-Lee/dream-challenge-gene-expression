8d838a3261e670bd6e01bb5fea8d944bc9454e70
DanYeong-Lee <ldy9381@snu.ac.kr>
2022-05-18 16:06:42 +0900

    Add DeepGRN model

---
+++ src/models/components/deepgrn.py
@@ -0,0 +1,126 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class SABlock(nn.Module):
+    def __init__(
+        self,
+        seq_len: int = 110,
+        input_dim: int = 320
+    ):
+        super().__init__()
+        self.att = nn.Linear(seq_len, seq_len)
+        
+    def forward(self, x):
+        # x: (N, C, L)
+        att_score = self.att(x)
+        att_score = F.softmax(att_score, dim=2)  # (N, C, L)
+        att_score = att_score.mean(dim=1, keepdims=True)  # (N, 1, L)
+        weighted = torch.mul(x, att_score)  # (N, C, L)
+        out = weighted.sum(dim=2)  # (N, C)
+        
+        return out
+
+    
+class MHABlock(nn.Module):
+    def __init__(
+        self,
+        input_dim: int = 320,
+        qkv_dim: int = 320,
+        num_heads: int = 4
+    ):
+        super().__init__()
+        self.q = nn.Linear(input_dim, qkv_dim)
+        self.k = nn.Linear(input_dim, qkv_dim)
+        self.v = nn.Linear(input_dim, qkv_dim)
+        self.mha = nn.MultiheadAttention(embed_dim=qkv_dim, num_heads=num_heads, batch_first=True)
+        
+    def forward(self, x):
+        # x: (N, L, C)
+        query = self.q(x)  # (N, L, C)
+        key = self.k(x)    # (N, L, C)
+        value = self.v(x)  # (N, L, C)
+        out, _ = self.mha(query, key, value, need_weights=False)  # (N, L, C)
+        
+        return out
+
+    
+class ConvBlock(nn.Module):
+    def __init__(
+        self,
+        input_dim: int = 4,
+        out_dim: int = 320,
+        kernel_size: int = 15,
+        pool_size: int = 3,
+        dropout: float = 0.2,
+    ):
+        super().__init__()
+        self.main = nn.Sequential(
+            nn.Conv1d(in_channels=input_dim, out_channels=out_dim, kernel_size=kernel_size, padding="same"),
+            nn.ReLU(),
+            nn.MaxPool1d(pool_size),
+            nn.Dropout(dropout)
+        )
+    
+    def forward(self, x):
+        # x: (N, C, L)
+        
+        return self.main(x)
+
+    
+class DeepGRN(nn.Module):
+    def __init__(
+        self,
+        conv_out_dim: int = 320,
+        conv_kernel_size: int = 15,
+        pool_size: int = 3,
+        lstm_hidden_dim: int = 160,
+        num_heads: int = 4,
+        fc_hidden_dim: int = 64,
+        dropout1: float = 0.2,
+        dropout2: float = 0.5
+    ):
+        super().__init__()
+        pool_out_len = int(1 + ((110 - pool_size) / pool_size))
+        fc_input_dim = lstm_hidden_dim * 2 * pool_out_len
+        
+        self.conv_block = ConvBlock(4, conv_out_dim, conv_kernel_size, pool_size, dropout1)
+        self.lstm = nn.LSTM(input_size=conv_out_dim, hidden_size=lstm_hidden_dim, bidirectional=True, batch_first=True)
+        self.sa_layer = SABlock(pool_out_len, lstm_hidden_dim * 2)
+        self.mha_layer = MHABlock(lstm_hidden_dim * 2, lstm_hidden_dim * 2, num_heads)
+        
+        self.sa_fc = nn.Sequential(
+            nn.Flatten(),
+            nn.Dropout(dropout2),
+            nn.Linear(lstm_hidden_dim * 2, fc_hidden_dim),
+            nn.ReLU(),
+            nn.Linear(fc_hidden_dim, fc_hidden_dim),
+            nn.ReLU(),
+            nn.Linear(fc_hidden_dim, 1)
+        )
+        
+        self.mha_fc = nn.Sequential(
+            nn.Flatten(),
+            nn.Dropout(dropout2),
+            nn.Linear(fc_input_dim, fc_hidden_dim),
+            nn.ReLU(),
+            nn.Linear(fc_hidden_dim, fc_hidden_dim),
+            nn.ReLU(),
+            nn.Linear(fc_hidden_dim, 1)
+        )
+        
+    def forward(self, x):
+        # x: (N, L, C)
+        x = x.transpose(1, 2)  # (N, C, L)
+        x = self.conv_block(x)  # (N, C, L)
+        x = x.transpose(1, 2)  # (N, L, C)
+        x, (h, c) = self.lstm(x)  # (N, L, C)
+        
+        sa_in = x.transpose(1, 2)  # (N, C, L)
+        sa_out = self.sa_fc(self.sa_layer(sa_in))
+        mha_out = self.mha_fc(self.mha_layer(x))
+        out = (sa_out + mha_out) / 2
+        
+        return out.squeeze()
