c880fba2b563e7d1e1186f3b383d51c154f28720
DanYoung-Lee <ldy9381@snu.ac.kr>
2022-06-16 20:06:10 +0900

    Add DeepFamLight & Fix correlation metric to be computed at every epoch

--- src/models/model.py
+++ src/models/model.py
@@ -1,4 +1,7 @@
 from typing import Any, List
+import json
+from collections import OrderedDict
+import numpy as np
 import pandas as pd
 
 import torch
@@ -60,37 +63,52 @@ class MainNet(LightningModule):
     
     def training_step(self, batch, batch_idx):
         loss, preds, targets = self.step(batch)
-        spearman = self.train_spearman(preds, targets)
-        pearson = self.train_pearson(preds, targets)
-        r2 = self.train_r2(preds, targets)
-        metrics = {"train/loss": loss, "train/spearman": spearman, "train/pearson": pearson, "train/r2": r2}
+        batch_spearman = self.train_spearman(preds, targets)
+        batch_pearson = self.train_pearson(preds, targets)
+        batch_r2 = self.train_r2(preds, targets)
+        metrics = {"train/loss_batch": loss}
         self.log_dict(metrics, on_step=False, on_epoch=True, prog_bar=True)
         
         return loss
     
-    def training_step_end(self, outputs):
+    def training_epoch_end(self, outputs):
+        # get metric from current epoch
+        epoch_spearman = self.train_spearman.compute()
+        epoch_pearson = self.train_pearson.compute()
+        epoch_r2 = self.train_r2.compute()
+        
+        # log epoch metrics
+        metrics = {"train/spearman": epoch_spearman, "train/pearson": epoch_pearson, "train/r2": epoch_r2}
+        self.log_dict(metrics, on_step=False, on_epoch=True, prog_bar=True)
+        
+        # reset metrics
         self.train_spearman.reset()
         self.train_pearson.reset()
         self.train_r2.reset()
-    
+        
     def validation_step(self, batch, batch_idx):
         loss, preds, targets = self.step(batch)
-        spearman = self.val_spearman(preds, targets)
-        pearson = self.val_pearson(preds, targets)
-        r2 = self.val_r2(preds, targets)
-        metrics = {"val/loss": loss, "val/spearman": spearman, "val/pearson": pearson, "val/r2": r2}
+        self.val_spearman.update(preds, targets)
+        self.val_pearson.update(preds, targets)
+        self.val_r2.update(preds, targets)
+        
+        metrics = {"val/loss": loss}
         self.log_dict(metrics, on_step=False, on_epoch=True, prog_bar=True)
         
     def validation_epoch_end(self, outputs):
         # get val metric from current epoch
-        spearman = self.val_spearman.compute()
-        pearson = self.val_pearson.compute()
-        r2 = self.val_r2.compute()
+        epoch_spearman = self.val_spearman.compute()
+        epoch_pearson = self.val_pearson.compute()
+        epoch_r2 = self.val_r2.compute()
+        
+        # log epoch metrics
+        metrics = {"val/spearman": epoch_spearman, "val/pearson": epoch_pearson, "val/r2": epoch_r2}
+        self.log_dict(metrics, on_epoch=True, prog_bar=True)
         
         # log best metric
-        self.val_spearman_best.update(spearman)
-        self.val_pearson_best.update(pearson)
-        self.val_r2_best.update(r2)
+        self.val_spearman_best.update(epoch_spearman)
+        self.val_pearson_best.update(epoch_pearson)
+        self.val_r2_best.update(epoch_r2)
         self.log("val/spearman_best", self.val_spearman_best.compute(), on_epoch=True, prog_bar=True)
         self.log("val/pearson_best", self.val_pearson_best.compute(), on_epoch=True, prog_bar=True)
         self.log("val/r2_best", self.val_r2_best.compute(), on_epoch=True, prog_bar=True)
@@ -102,11 +120,22 @@ class MainNet(LightningModule):
         
     def test_step(self, batch, batch_idx):
         loss, preds, targets = self.step(batch)
-        spearman = self.test_spearman(preds, targets)
-        pearson = self.test_pearson(preds, targets)
-        r2 = self.test_r2(preds, targets)
-        metrics = {"test/loss": loss, "test/spearman": spearman, "test/pearson": pearson, "test/r2": r2}
+        self.test_spearman.update(preds, targets)
+        self.test_pearson.update(preds, targets)
+        self.test_r2.update(preds, targets)
+        metrics = {"test/loss": loss}
         self.log_dict(metrics, on_step=False, on_epoch=True, prog_bar=True)
+        
+    def test_epoch_end(self, outputs):
+        # get val metric from current epoch
+        epoch_spearman = self.test_spearman.compute()
+        epoch_pearson = self.test_pearson.compute()
+        epoch_r2 = self.test_r2.compute()
+        
+        # log epoch metrics
+        metrics = {"test/spearman": epoch_spearman, "test/pearson": epoch_pearson, "test/r2": epoch_r2}
+        self.log_dict(metrics, on_epoch=True, prog_bar=True)
+        
         
     def predict_step(self, batch, batch_idx):
         _, preds, _ = self.step(batch)
@@ -114,10 +143,20 @@ class MainNet(LightningModule):
         return preds
     
     def on_predict_epoch_end(self, outputs):
-        whole_preds = torch.cat(outputs[0])
-        df = pd.read_csv("/data/project/ddp/data/dream/test_sequences.txt", sep="\t", names=["seq", "target"])
-        df.target = whole_preds
-        df.to_csv("../../../../../submission.txt", sep="\t", index=False, header=None)
+        with open("../../../../../sample_submission.json", "r") as f:
+            ground = json.load(f)
+    
+        indices = np.array([int(indice) for indice in list(ground.keys())])
+        PRED_DATA = OrderedDict()
+        Y_pred = np.array(torch.cat(outputs[0]))
+
+        for i in indices:
+            PRED_DATA[str(i)] = float(Y_pred[i])
+        
+        
+        with open("../../../../../submission.json", "w") as f:
+            json.dump(PRED_DATA, f)
+
         print("Saved submission file!")
     
     def configure_optimizers(self):
@@ -190,11 +229,6 @@ class ConjoinedNet_CA(ConjoinedNet):
         self.min_lr = min_lr
         self.warmup_steps = warmup_steps
         self.gamma = gamma
-        
-    
-    def training_epoch_end(self, outputs):
-        sch = self.lr_schedulers()
-        sch.step()
     
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(
@@ -237,11 +271,6 @@ class ConjoinedNet_AW_CA(ConjoinedNet):
         self.min_lr = min_lr
         self.warmup_steps = warmup_steps
         self.gamma = gamma
-        
-    
-    def training_epoch_end(self, outputs):
-        sch = self.lr_schedulers()
-        sch.step()
     
     def configure_optimizers(self):
         optimizer = torch.optim.AdamW(
