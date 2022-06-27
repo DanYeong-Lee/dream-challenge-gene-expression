2cbc008173d7947ae43add2346a92501357bd1ae
DanYeong-Lee <ldy9381@snu.ac.kr>
2022-06-27 23:19:25 +0900

    Add datamodule v2

--- src/datamodules/components/dataset.py
+++ src/datamodules/components/dataset.py
@@ -42,6 +42,48 @@ class OneHotDataset(Dataset):
         y = torch.tensor(float(target), dtype=torch.float32)
         
         return X, X_rev, y
+    
+
+class OneHotDataset_v2(Dataset):
+    def __init__(
+        self, 
+        df
+    ):
+        self.records = df.to_records()
+        self.base2vec = {
+            "A": [1., 0., 0., 0.],
+            "T": [0., 1., 0., 0.],
+            "C": [0., 0., 1., 0.],
+            "G": [0., 0., 0., 1.],
+            "N": [0.25, 0.25, 0.25, 0.25],
+            "X": [0., 0., 0., 0.]
+        }
+    
+    def seq2mat(self,seq, max_len=110):
+        if len(seq) > max_len:
+            seq = seq[len(seq) - max_len :]
+        else:
+            seq = "X" * (max_len - len(seq)) + seq
+        mat = torch.tensor(list(map(lambda x: self.base2vec[x], seq)), dtype=torch.float32)
+        return mat
+    
+    def reverse_complement(self, fwd_tensor):
+        temp = fwd_tensor.flip(0)
+        rev_tensor = temp.index_select(dim=1, index=torch.LongTensor([1, 0, 3, 2]))
+        
+        return rev_tensor
+
+    def __len__(self):
+        return len(self.records)
+    
+    def __getitem__(self, idx):
+        _, seq, target = self.records[idx]
+        X = self.seq2mat(seq)
+        X_rev = self.reverse_complement(X)
+        y = torch.tensor(float(target), dtype=torch.float32)
+        
+        return X, X_rev, y
+    
 
     
 class IndexDataset(Dataset):
