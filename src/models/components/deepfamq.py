c68dc4e117b697c8fe38247999cbb1f164a212ac
DanYeong-Lee <ldy9381@snu.ac.kr>
2022-07-03 14:28:57 +0900

    Add multilayer LSTM

--- src/models/components/deepfamq.py
+++ src/models/components/deepfamq.py
@@ -95,6 +95,111 @@ class DeepFamQ(nn.Module):
         x = x.squeeze()
         
         return x
+
+
+class DeepFamQ_multilayerConv(nn.Module):
+    def __init__(
+        self,
+        conv_out_dim: int = 320,
+        conv_kernel_size: List = [10, 15],
+        pool_size: int = 3,
+        lstm_hidden_dim: int = 320,
+        fc_hidden_dim: int = 64,
+        dropout1: float = 0.2,
+        dropout2: float = 0.5
+    ):
+        super().__init__()
+        pool_out_len = int(1 + ((110 - pool_size) / pool_size))
+        fc_input_dim = lstm_hidden_dim * 2 * pool_out_len
+        conv_each_dim = int(conv_out_dim / len(conv_kernel_size))
+        
+        self.conv_blocks1 = nn.ModuleList(
+            [ConvBlock(4, conv_each_dim, k, pool_size, dropout1) for k in conv_kernel_size]
+        )
+        self.conv_blocks2 = nn.ModuleList(
+            [ConvBlock(conv_out_dim, conv_each_dim, k, pool_size, dropout1) for k in conv_kernel_size]
+        )
+        self.lstm = nn.LSTM(input_size=conv_out_dim, hidden_size=lstm_hidden_dim, bidirectional=True)
+        
+        self.fc = nn.Sequential(
+            nn.Flatten(),
+            nn.Dropout(dropout2),
+            nn.Linear(fc_input_dim, fc_hidden_dim),
+            nn.ReLU(),
+            nn.Linear(fc_hidden_dim, fc_hidden_dim),
+            nn.ReLU(),
+            nn.Linear(fc_hidden_dim, 1)
+        )
+        
+    def forward(self, x):
+        # x: (N, L, C)
+        x = x.transpose(1, 2)  # (N, C, L)
+        conv_outs1 = []
+        for conv in self.conv_blocks1:
+            conv_outs1.append(conv(x))
+        x = torch.cat(conv_outs1, dim=1)  # (N, C, L)
+        conv_outs2 = []
+        for conv in self.conv_blocks2:
+            conv_outs2.append(conv(x))
+        x = torch.cat(conv_outs2, dim=1)  # (N, C, L)
+        
+        x = x.permute(2, 0, 1)  # (L, N, C)
+        x, (h, c) = self.lstm(x)  # (L, N, C)
+        x = x.transpose(0, 1)  # (N, L, C)
+        x = self.fc(x)
+        x = x.squeeze()
+        
+        return x
+    
+    
+class DeepFamQ_multilayerLSTM(nn.Module):
+    def __init__(
+        self,
+        conv_out_dim: int = 320,
+        conv_kernel_size: List = [10, 15],
+        pool_size: int = 3,
+        lstm_hidden_dim: int = 320,
+        lstm_layers: int = 2,
+        fc_hidden_dim: int = 64,
+        dropout1: float = 0.2,
+        dropout2: float = 0.5
+    ):
+        super().__init__()
+        pool_out_len = int(1 + ((110 - pool_size) / pool_size))
+        fc_input_dim = lstm_hidden_dim * 2 * pool_out_len
+        conv_each_dim = int(conv_out_dim / len(conv_kernel_size))
+        
+        self.conv_blocks = nn.ModuleList([ConvBlock(4, conv_each_dim, k, pool_size, dropout1) for k in conv_kernel_size])
+        self.lstm = nn.LSTM(input_size=conv_out_dim, 
+                            hidden_size=lstm_hidden_dim, 
+                            bidirectional=True, 
+                            num_layers=lstm_layers, 
+                            dropout=dropout2)
+        
+        self.fc = nn.Sequential(
+            nn.Flatten(),
+            nn.Dropout(dropout2),
+            nn.Linear(fc_input_dim, fc_hidden_dim),
+            nn.ReLU(),
+            nn.Linear(fc_hidden_dim, fc_hidden_dim),
+            nn.ReLU(),
+            nn.Linear(fc_hidden_dim, 1)
+        )
+        
+    def forward(self, x):
+        # x: (N, L, C)
+        x = x.transpose(1, 2)  # (N, C, L)
+        conv_outs = []
+        for conv in self.conv_blocks:
+            conv_outs.append(conv(x))
+        x = torch.cat(conv_outs, dim=1)  # (N, C, L)
+        x = x.permute(2, 0, 1)  # (L, N, C)
+        x, (h, c) = self.lstm(x)  # (L, N, C)
+        x = x.transpose(0, 1)  # (N, L, C)
+        x = self.fc(x)
+        x = x.squeeze()
+        
+        return x
     
 
 class DeepFamQ_MLPDO(nn.Module):
