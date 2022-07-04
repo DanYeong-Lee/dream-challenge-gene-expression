fc96b95d36a905ce9ec6a16d3134727fb8aeb7a9
DanYeong-Lee <ldy9381@snu.ac.kr>
2022-07-04 15:37:21 +0900

    Add DeepFamQ-CRRC

--- src/models/components/deepfamq_crc.py
+++ src/models/components/deepfamq_crc.py
@@ -90,23 +90,27 @@ class DeepFamQ_CRRC(nn.Module):
         lstm_layers: int = 2,
         fc_hidden_dim: int = 64,
         dropout1: float = 0.2,
-        dropout2: float = 0.5
+        dropout2: float = 0.5,
+        dropout3: float = 0.5
     ):
         super().__init__()
         pool_out_len = int(1 + ((110 - pool_size) / pool_size))
         fc_input_dim = lstm_hidden_dim * 2 * pool_out_len
         conv_each_dim = int(conv_out_dim / len(conv_kernel_size))
         
-        self.conv_blocks = nn.ModuleList([ConvBlock(4, conv_each_dim, k, pool_size, dropout1) for k in conv_kernel_size])
+        self.conv_blocks1 = nn.ModuleList([ConvBlock(4, conv_each_dim, k, pool_size, dropout1) for k in conv_kernel_size])
+        
         self.lstm = nn.LSTM(input_size=conv_out_dim, 
                             hidden_size=lstm_hidden_dim, 
                             bidirectional=True, 
                             num_layers=lstm_layers, 
                             dropout=dropout2)
         
+        self.conv_blocks2 = nn.ModuleList([ConvBlock(lstm_hidden_dim * 2, conv_each_dim, k, pool_size, dropout1) for k in conv_kernel_size])
+        
         self.fc = nn.Sequential(
             nn.Flatten(),
-            nn.Dropout(dropout2),
+            nn.Dropout(dropout3),
             nn.Linear(fc_input_dim, fc_hidden_dim),
             nn.ReLU(),
             nn.Linear(fc_hidden_dim, fc_hidden_dim),
@@ -118,12 +122,16 @@ class DeepFamQ_CRRC(nn.Module):
         # x: (N, L, C)
         x = x.transpose(1, 2)  # (N, C, L)
         conv_outs = []
-        for conv in self.conv_blocks:
+        for conv in self.conv_blocks1:
             conv_outs.append(conv(x))
         x = torch.cat(conv_outs, dim=1)  # (N, C, L)
         x = x.permute(2, 0, 1)  # (L, N, C)
         x, (h, c) = self.lstm(x)  # (L, N, C)
-        x = x.transpose(0, 1)  # (N, L, C)
+        x = x.permute(1, 2, 0)  # (N, C, L)
+        conv_outs = []
+        for conv in self.conv_blocks2:
+            conv_outs.append(conv(x))
+        x = torch.cat(conv_outs, dim=1)  # (N, C, L)
         x = self.fc(x)
         x = x.squeeze()
         
