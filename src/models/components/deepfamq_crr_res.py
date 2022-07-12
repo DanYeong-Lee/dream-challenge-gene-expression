050f745735272a4fa5081462400886ef0175dfb3
DanYoung-Lee <ldy9381@snu.ac.kr>
2022-07-13 00:54:32 +0900

    asd

--- src/models/components/deepfamq_crr_res.py
+++ src/models/components/deepfamq_crr_res.py
@@ -48,12 +48,12 @@ class DeepFamQ_CRR_Res(nn.Module):
         
         self.conv_blocks = nn.ModuleList([ConvBlock(4, conv_each_dim, k, pool_size, dropout1) for k in conv_kernel_size])
         self.lstm1 = nn.LSTM(input_size=conv_out_dim, hidden_size=lstm_hidden_dim, bidirectional=True)
-        self.do = nn.Dropout(dropout)
+        self.do = nn.Dropout(dropou2)
         self.lstm2 = nn.LSTM(input_size=lstm_hidden_dim * 2, hidden_size=lstm_hidden_dim, bidirectional=True)
         
         self.fc = nn.Sequential(
             nn.Flatten(),
-            nn.Dropout(dropout5),
+            nn.Dropout(dropout3),
             nn.Linear(fc_input_dim, fc_hidden_dim),
             nn.ReLU(),
             nn.Linear(fc_hidden_dim, fc_hidden_dim),
