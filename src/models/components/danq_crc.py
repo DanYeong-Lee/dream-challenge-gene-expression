c0df0e59262adec7e361c6bf9c3a65e08332791c
DanYeong-Lee <ldy9381@snu.ac.kr>
2022-07-15 20:07:27 +0900

    asd

--- src/models/components/danq_crc.py
+++ src/models/components/danq_crc.py
@@ -3,30 +3,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
-class ConvBlock_bn(nn.Module):
-    def __init__(
-        self,
-        input_dim: int = 4,
-        out_dim: int = 320,
-        kernel_size: int = 26,
-        pool_size: int = 3,
-        dropout: float = 0.2
-    ):
-        super().__init__()
-        self.main = nn.Sequential(
-            nn.Conv1d(in_channels=input_dim, out_channels=out_dim, kernel_size=kernel_size),
-            nn.BatchNorm1d(out_dim),
-            nn.ReLU(),
-            nn.Dropout(dropout),
-            nn.MaxPool1d(pool_size)
-        )
-    
-    def forward(self, x):
-        # x: (N, C, L)
-        
-        return self.main(x)
-
     
 class ConvBlock(nn.Module):
     def __init__(
@@ -39,7 +15,7 @@ class ConvBlock(nn.Module):
     ):
         super().__init__()
         self.main = nn.Sequential(
-            nn.Conv1d(in_channels=input_dim, out_channels=out_dim, kernel_size=kernel_size),
+            nn.Conv1d(in_channels=input_dim, out_channels=out_dim, kernel_size=kernel_size, padding="same"),
             nn.ReLU(),
             nn.MaxPool1d(pool_size),
             nn.Dropout(dropout)
@@ -51,7 +27,7 @@ class ConvBlock(nn.Module):
         return self.main(x)
 
 
-class DanQ(nn.Module):
+class DanQ_CRC(nn.Module):
     def __init__(
         self,
         conv_out_dim: int = 320,
@@ -65,11 +41,14 @@ class DanQ(nn.Module):
         super().__init__()
         conv_out_len = 110 - conv_kernel_size + 1
         pool_out_len = int(1 + ((conv_out_len - pool_size) / pool_size))
-        fc_input_dim = lstm_hidden_dim * 2 * pool_out_len
+        fc_input_dim = lstm_hidden_dim * pool_out_len
         
-        self.conv_block = ConvBlock(4, conv_out_dim, conv_kernel_size, pool_size, dropout1)
-
+        self.conv_block1 = ConvBlock(4, conv_out_dim, conv_kernel_size, pool_size, dropout1)
+        
         self.lstm = nn.LSTM(input_size=conv_out_dim, hidden_size=lstm_hidden_dim, bidirectional=True)
+        
+        self.conv_block2 = ConvBlock(lstm_hidden_dim * 2, lstm_hidden_dim, conv_kernel_size, pool_size, dropout1)
+        
         self.fc = nn.Sequential(
             nn.Flatten(),
             nn.Dropout(dropout2),
@@ -85,9 +64,10 @@ class DanQ(nn.Module):
         # Only use forward strand data
         
         x = fwd_x.transpose(1, 2)  # (N, C, L)
-        x = self.conv_block(x)
+        x = self.conv_block1(x)
         x = x.permute(2, 0, 1)  # (L, N, C)
         x, (h, c) = self.lstm(x)
-        x = x.transpose(0, 1)  # (N, L, C)
+        x = x.permute(1, 2, 0)  # (N, C, L)
+        x - self.conv_block2(x)
         x = self.fc(x).squeeze(-1)
         return x
