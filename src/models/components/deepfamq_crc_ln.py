8659670658513002a27e9a118e9620ebdf339665
DanYoung-Lee <ldy9381@snu.ac.kr>
2022-07-20 17:55:49 +0900

    asd

--- src/models/components/deepfamq_crc_ln.py
+++ src/models/components/deepfamq_crc_ln.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from einops import rearrange
-from einops.layer.torch import Rearrange
+from einops.layers.torch import Rearrange
 
 class ConvBlock(nn.Module):
     def __init__(
