a90c5cd646391bc9aedb7e30db5709a4cd5df3ef
DanYeong-Lee <ldy9381@snu.ac.kr>
2022-07-25 11:36:32 +0900

    asd

--- src/models/components/deepfamq_crc_big.py
+++ src/models/components/deepfamq_crc_big.py
@@ -1,17 +1,20 @@
+from typing import List
+import math
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from einops import rearrange
 
-    
+
 class ConvBlock(nn.Module):
     def __init__(
         self,
         input_dim: int = 4,
         out_dim: int = 320,
-        kernel_size: int = 26,
+        kernel_size: int = 15,
         pool_size: int = 3,
-        dropout: float = 0.2
+        dropout: float = 0.2,
     ):
         super().__init__()
         self.main = nn.Sequential(
@@ -27,11 +30,11 @@ class ConvBlock(nn.Module):
         return self.main(x)
 
 
-class DanQ_CRC(nn.Module):
+class DeepFamQ_CRC(nn.Module):
     def __init__(
         self,
         conv_out_dim: int = 320,
-        conv_kernel_size: int = 26,
+        conv_kernel_size: List = [9, 15],
         pool_size: int = 3,
         lstm_hidden_dim: int = 320,
         fc_hidden_dim: int = 64,
@@ -39,15 +42,17 @@ class DanQ_CRC(nn.Module):
         dropout2: float = 0.5
     ):
         super().__init__()
-        conv_out_len = 110
-        pool_out_len = int(1 + ((conv_out_len - pool_size) / pool_size))
-        fc_input_dim = lstm_hidden_dim * pool_out_len
+        pool_out_len = int(1 + ((110 - pool_size) / pool_size))
+        pool_out_len = int(1 + ((pool_out_len - pool_size) / pool_size))
+        fc_input_dim = lstm_hidden_dim * 2 * pool_out_len
         
-        self.conv_block1 = ConvBlock(4, conv_out_dim, conv_kernel_size, pool_size, dropout1)
+        conv_each_dim = int(conv_out_dim / len(conv_kernel_size))
+        self.conv_blocks1 = nn.ModuleList([ConvBlock(4, conv_each_dim, k, pool_size, dropout1) for k in conv_kernel_size])
         
         self.lstm = nn.LSTM(input_size=conv_out_dim, hidden_size=lstm_hidden_dim, bidirectional=True)
         
-        self.conv_block2 = ConvBlock(lstm_hidden_dim * 2, lstm_hidden_dim, conv_kernel_size, pool_size, dropout1)
+        conv_each_dim = int(lstm_hidden_dim * 2 / len(conv_kernel_size))
+        self.conv_blocks2 = nn.ModuleList([ConvBlock(lstm_hidden_dim * 2, conv_each_dim, k, pool_size, dropout1) for k in conv_kernel_size])
         
         self.fc = nn.Sequential(
             nn.Flatten(),
@@ -59,15 +64,26 @@ class DanQ_CRC(nn.Module):
             nn.Linear(fc_hidden_dim, 1)
         )
         
-    def forward(self, fwd_x):
-        # x: (N, L, C)
-        # Only use forward strand data
+    def forward(self, x):
+        x = rearrange(x, "N L C -> N C L")
         
-        x = fwd_x.transpose(1, 2)  # (N, C, L)
-        x = self.conv_block1(x)
-        x = x.permute(2, 0, 1)  # (L, N, C)
+        conv_outs = []
+        for conv in self.conv_blocks1:
+            conv_outs.append(conv(x))
+        x = torch.cat(conv_outs, dim=1)
+        
+        x = rearrange(x, "N C L -> L N C")
         x, (h, c) = self.lstm(x)
-        x = x.permute(1, 2, 0)  # (N, C, L)
-        x = self.conv_block2(x)
-        x = self.fc(x).squeeze(-1)
+        
+        x = rearrange(x, "L N C -> N C L")
+        
+        conv_outs = []
+        for conv in self.conv_blocks2:
+            conv_outs.append(conv(x))
+        x = torch.cat(conv_outs, dim=1)
+        
+        x = self.fc(x)
+        x = rearrange(x, "N 1 -> N")
+        
         return x
+    
